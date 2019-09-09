a900d0767e32a8d189b8c814cee384bb07db26ae
André Fernandes <andrefcasimiro@awayco.com>
2019-09-09 02:20:24 +0100

    Updates

---
+++ precache-manifest.a2231b1a03435d5c8ce0eaa9ec075303.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5916cc0c7a2dd766870fb0417de878",
+    "url": "/music_machine/index.html"
+  },
+  {
+    "revision": "06e6f1d3f14c9064ce2f",
+    "url": "/music_machine/static/css/main.b1af37ee.chunk.css"
+  },
+  {
+    "revision": "ad9bce64cecdeb41e98f",
+    "url": "/music_machine/static/js/2.0e42d228.chunk.js"
+  },
+  {
+    "revision": "06e6f1d3f14c9064ce2f",
+    "url": "/music_machine/static/js/main.3f3a10c6.chunk.js"
+  },
+  {
+    "revision": "077a059f455e1ff2f7f8",
+    "url": "/music_machine/static/js/runtime~main.de1d260f.js"
+  },
+  {
+    "revision": "a8a6060842a708807d687bd9019bf5c9",
+    "url": "/music_machine/static/media/Bass.a8a60608.wav"
+  },
+  {
+    "revision": "ce5193f619d602e79514797449d40404",
+    "url": "/music_machine/static/media/Clap.ce5193f6.WAV"
+  },
+  {
+    "revision": "bf48fb2070c8a8a0d6430cff4bd05414",
+    "url": "/music_machine/static/media/ClosedHat.bf48fb20.WAV"
+  },
+  {
+    "revision": "6471470d1abf10833b1f414d6b9a9949",
+    "url": "/music_machine/static/media/Kick.6471470d.WAV"
+  },
+  {
+    "revision": "67b2afb38f72359d1d4cb3a722d7d185",
+    "url": "/music_machine/static/media/OpenHat.67b2afb3.WAV"
+  },
+  {
+    "revision": "9c1dc1af0178538e3b0a95cdc759a2c0",
+    "url": "/music_machine/static/media/PAD.9c1dc1af.mp3"
+  },
+  {
+    "revision": "d76d77c92cca3147e1512f25b271da98",
+    "url": "/music_machine/static/media/Snare.d76d77c9.WAV"
+  }
+]);
