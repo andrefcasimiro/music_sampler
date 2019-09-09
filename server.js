a185f3e01425f7965a960851ce059f384f2cb8a4
André Fernandes <andrefcasimiro@awayco.com>
2019-09-09 12:10:35 +0100

    ...

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express')
 var app = express()
 var multer = require('multer')
 var cors = require('cors')
-var path = require('path')
-var proxy = require('http-proxy-middleware')
 
 var whitelist = [
   'https://react-drum-machine-sampler.herokuapp.com/',
@@ -23,23 +21,6 @@ var corsOptions = {
   },
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-
-
-app.use('/upload', proxy({
-  target: 'https://react-drum-machine-sampler.herokuapp.com',
-  changeOrigin: true,
-}));
-
-app.use('/upload', proxy({
-  target: 'http://localhost:3000/',
-  changeOrigin: true,
-}));
-
-app.use('*', proxy({
-  target: 'https://react-drum-machine-sampler.herokuapp.com',
-  changeOrigin: true,
-}));
-
 
 app.use(cors(corsOptions))
 
@@ -65,8 +46,6 @@ app.post('/upload', function (req, res) {
     return res.status(200).send(req.file)
   })
 })
-
-
 
 app.listen(8000, function() {
   console.log('Running on port 8000 :)')
